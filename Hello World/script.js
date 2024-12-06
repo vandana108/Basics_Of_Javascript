4a25e41956226b1f944cc3964b76679d6eb87cb3
vandana108 <thevandanarai108@gmail.com>
2024-12-06 18:25:09 +0530

    Add initial code for basics of Javascript

---
+++ Hello World/script.js
@@ -0,0 +1,11 @@
+console.log('Hello world!')
+  console.log(5+6-5*4);
+  let username = 'Vandana'// need to initialze before print
+  let fullname = 'My fullname name is vandana rai'
+  let age  = 25;
+  let isHappy=true;
+  let userIntro = 'Hi my name is'+' '+username+' '+fullname+' '+age
+ { 
+    var a= 'Adarsh'//a block of code we don't need to initialize before executing
+  }
+  console.log(typeof 'Anjali')  
