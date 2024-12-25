76298bd5b678f3919f4c88b8e222c7fde4399f63
vandana108 <thevandanarai108@gmail.com>
2024-12-25 22:02:32 +0530

    Practice on Function

--- Function/Function in javascript/script(Practice).js
+++ Function/Function in javascript/script(Practice).js
@@ -1,18 +1,64 @@
-function introduceMe()
+// function introduceMe()
+// {
+//   console.log('hi');
+//   console.log('My name is Vandana');
+//   console.log('I am Software Developer');
+//   return 'Vandnaa'
+// }
+// console.log('*****************************');
+
+
+// function introduceName(username, userage, userId)
+// {
+//   console.log('hi');
+//   console.log(username);
+//   console.log(userId);
+
+// }
+// introduceName('Vandnaa', 32, 53433);
+
+
+//add two numbers
+//declaration function
+function addTwoNumber(num1, num2)
 {
-  console.log('hi');
-  console.log('My name is Vandana');
-  console.log('I am Software Developer');
-  return 'Vandnaa'
+  return num1+num2;
 }
-console.log('*****************************');
+
+const answer = addTwoNumber(45,5);
+console.log(answer);
 
 
-function introduceName(username, userage, userId)
-{
-  console.log('hi');
-  console.log(username);
-  console.log(userId);
-
+//Anonymous function or  Expression function
+const hello = function(){
+  console.log('Hi, guys hope you are doing great');
 }
-introduceName('Vandnaa', 32, 53433);
+
+//Arrow function
+let result = () => {
+  console.log('This is my result');
+  return 8+4;
+}
+
+function func(a, b) {
+  let sum = a + b;
+  console.log(sum);
+  return sum;
+}
+console.log(func(1, 2));
+
+//Method 
+
+const some = {
+  location:function(){
+    //content
+    console.log('this is my location')
+  },
+  userName:function(){
+    console.log('my name is Utkarsh')
+  },
+  projectID:function()
+  {
+    console.log('this is my Project ID')
+  }
+}
