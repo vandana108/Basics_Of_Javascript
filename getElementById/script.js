6b15c0e3856d7378fee3e876796edcee084f270f
vandana108 <thevandanarai108@gmail.com>
2024-12-26 23:53:06 +0530

    Learn on-off feature

--- getElementById/script.js
+++ getElementById/script.js
@@ -3,3 +3,10 @@
 function changeContent() {
   document.getElementById("demo").innerHTML = "Hello JavaScript!";
 }
+function turnOnLight() {
+  document.getElementById('myImage').src = 'pic_bulbon.gif';
+}
+
+function turnOffLight() {
+  document.getElementById('myImage').src = 'pic_bulboff.gif';
+}
