9a8716e478b94371fa42e72ba4f530e823ff3c2b
vandana108 <thevandanarai108@gmail.com>
2024-12-26 23:35:19 +0530

    Practice getEventById

---
+++ Event Bubbling and Event Capturing/event.js
@@ -0,0 +1,21 @@
+// setTimeout(() => {
+//   console.log('Hi Vandana')
+// }, 2000);
+
+// const timer1= setTimeout('console.log("hi-2")',1000);
+// const timer2 = setTimeout('console.log("hi-3")',3000);
+// const timer3 = setTimeout(a, 3000, 'str', 99, 54,43, [1,4]);
+
+// const timer4 = setInterval(a, 3000, 'str', 99, 54,43, [1,4]);
+
+// const timer5 = setInterval('console.log("Hello guys")', 2000)//it will print on every 2 second
+// setTimeout('console.log("hi-4")',4000);
+
+// // clearInterval(timer1);
+// // clearTimeout(timer2);
+
+// function a(){//setTimeout as higher function
+//   console.log(arguments[4])
+//   // console.log('Hello World');
+// }
+
---
+++ getElementById/script.js
@@ -0,0 +1,5 @@
+
+// JavaScript to change the content of the paragraph
+function changeContent() {
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
