96ed499b8d914e6a0fbff09d2e74a5b000c89286
vandana108 <thevandanarai108@gmail.com>
2024-12-27 02:05:42 +0530

    Practice

--- getElementById/script.js
+++ getElementById/script.js
@@ -3,10 +3,26 @@
 function changeContent() {
   document.getElementById("demo").innerHTML = "Hello JavaScript!";
 }
-function turnOnLight() {
-  document.getElementById('myImage').src = 'pic_bulbon.gif';
+// function turnOnLight() {
+//   document.getElementById('myImage').src = 'pic_bulbon.gif';
+// }
+
+// function turnOffLight() {
+//   document.getElementById('myImage').src = 'pic_bulboff.gif';
+// }
+
+function bulbOn(){
+  document.getElementById('myImage').src='pic_bulbon.gif';
 }
 
-function turnOffLight() {
-  document.getElementById('myImage').src = 'pic_bulboff.gif';
+function bulbOff(){
+  document.getElementById("myImage").src="pic_bulboff.gif";
+}
+
+
+function wrongAnswer(){
+  document.getElementById('question').src="https://www.w3schools.com/js/exercise.asp?x=xrcise_intro1d";
+}
+function correctAnswer(){
+  document.getElementById('question').src="https://www.w3schools.com/js/exercise.asp?x=xrcise_intro1";
 }
