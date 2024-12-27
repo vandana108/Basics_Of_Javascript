6667cc39e106ac8a0454f2ebd563e987e1f194d8
vandana108 <thevandanarai108@gmail.com>
2024-12-27 23:41:36 +0530

    Practice Object in js

--- Object in javascript/script.js
+++ Object in javascript/script.js
@@ -1,4 +1,7 @@
 const user = {firstName: 'Akash'}
+
+
+
 const user2 ={
   firstName:'Vandana',
   "last-Name": 'singh',
@@ -11,7 +14,33 @@ const user2 ={
     state: 'uttar pradesh',
  },
 }
-console.log(user2.firstName);
-console.log(user2.lastName);
-console.log(user2["last-Name"]);
-console.log(user2.pata.populaton)
+// console.log(user2.firstName);
+// console.log(user2.lastName);
+// console.log(user2["last-Name"]);
+// console.log(user2.pata.populaton)
+
+
+const car = { type:"Fiat", model:"500", color:"white",
+   location:{
+      pin:"343443",
+      area:"good-Environment",
+   },
+
+}
+console.log(car.location.pin);
+//or
+console.log(car.location["pin"])
+// console.log(car.type);
+// console.log(car.location.pin)
+
+
+const myObj = {
+   name:"Kajal",
+   location: "Varanasi",
+   herCar: {
+      car1:"Ford",
+      car2:"Audi",
+      car3:"BMW",
+   },
+}
+document.getElementById("demo").innerHTML=myObj.herCar.car1;
