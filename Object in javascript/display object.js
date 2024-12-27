1f723188157bb4ea780f01e5f48c2838fc11f258
vandana108 <thevandanarai108@gmail.com>
2024-12-28 00:23:01 +0530

    display JavaScript objects

---
+++ Object in javascript/display object.js
@@ -0,0 +1,6 @@
+// Some solutions to display JavaScript objects are:
+
+// Displaying the Object Properties by name
+// Displaying the Object Properties in a Loop
+// Displaying the Object using Object.values()
+// Displaying the Object using JSON.stringify()
