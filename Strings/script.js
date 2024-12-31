fb2693fb2360c3966e09f5009d655a111fa4a59d
vandana108 <thevandanarai108@gmail.com>
2024-12-31 23:45:32 +0530

    JavaScript String slice

--- Strings/script.js
+++ Strings/script.js
@@ -1,15 +1,22 @@
-const message='Hello world'
-console.log("hi vandna")
+// const message='Hello world'
+// console.log("hi vandna")
 
-console.log(message.length);
-const width =+prompt("Please enter width")
-const height =+prompt("Please enter height")
+// console.log(message.length);
+// const width =+prompt("Please enter width")
+// const height =+prompt("Please enter height")
 
-document.write('this is the area'+width*height)
+// document.write('this is the area'+width*height)
 
-const user1=prompt('Enter your name')
-const age=prompt('Enter your age')
-if(age>18)
-{
-  document.write('You are eligible for Votes')
-}
+// const user1=prompt('Enter your name')
+// const age=prompt('Enter your age')
+// if(age>18)
+// {
+//   document.write('You are eligible for Votes')
+// }
+
+// JavaScript String slice()
+let text = "Apple, Banana, Kiwi";
+let parts = text.slice(7, 13);
+let str1 = parts;
+let str2 = "Vandana Rai"
+let name1 = str2.slice(1, 2);
