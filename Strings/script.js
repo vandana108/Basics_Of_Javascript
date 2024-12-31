b8b436d978571eb37f6044cc4854ec80e062985e
vandana108 <thevandanarai108@gmail.com>
2024-12-31 23:55:44 +0530

    string

--- Strings/script.js
+++ Strings/script.js
@@ -18,5 +18,18 @@
 let text = "Apple, Banana, Kiwi";
 let parts = text.slice(7, 13);
 let str1 = parts;
-let str2 = "Vandana Rai"
+let str2 = "Vandana Rai";
 let name1 = str2.slice(1, 2);
+
+
+// If we omit the second parameter, the method will slice out the rest of the string:
+let str3 = text.slice(7);
+
+// If a parameter is negative, the position is counted from the end of the string:
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12);
+
+// This example slices out a portion of a string from position -12 to position -6:
+
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12, -6);
