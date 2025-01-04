01d43fa130f49f0d5b38cbccca20f04a9e1abda7
vandana108 <thevandanarai108@gmail.com>
2025-01-05 00:04:28 +0530

    DOM

--- DOM Practice by Procodrr/script.js
+++ DOM Practice by Procodrr/script.js
@@ -1,4 +1,10 @@
-function sayHi()
-{
-  document.body.children[4].src='http://www.google.com'
-}
+// function sayHi()
+// {
+//   document.body.children[4].src='http://www.google.com'
+// }
+
+// const allImages= document.getElementsByTagName('img')
+// const cssImage = document.getElementsByClassName('css-image')[0]
+
+// const cssImage = document.querySelector('.css-image')
+const cssImage = document.querySelectorAll('.css-image')
